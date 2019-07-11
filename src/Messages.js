dc5289d3cbdd4c23bf22c08c638ad4254e009683
Ashley <36495929+myashley@users.noreply.github.com>
2019-07-11 15:09:16 +1000

    Add render message function to Messages component

--- src/Messages.js
+++ src/Messages.js
@@ -1,11 +1,33 @@
 import React, {component} from "react"
+import { memberExpression } from "@babel/types";
 
 class Messages extends Component {
+
+    renderMessage(message) {
+        const {member, text} = message
+        const {currrentMember} = this.props
+        const messageFromMe = member.id === currrentMember.id
+        const className = messageFromMe ? "Messages-message currentMember" : "Messages-message"
+
+        return (
+            <li className={className}>
+                <span className="avatar" style={{backgroundColor: member.clientData.color}} />
+                <div className="Message-content">
+                    <div className="username">
+                        {memberExpression.clientData.username}
+                    </div>
+                    <div className="text">{text}</div>
+                </div>
+            </li>
+        )
+    }
+
+
     render() {
         const {messages} = this.props
         return (
             <ul className="Messages-list">
-                {messages.map(message => this.renderMessage(message))}
+                {messages.map(m => this.renderMessage(m))}
             </ul>
         )
     }
