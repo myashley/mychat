7edffb9376f319fdf9d8f5128e80690feb610afa
Ashley <36495929+myashley@users.noreply.github.com>
2019-07-11 16:28:50 +1000

    Add Input component

---
+++ src/Input.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+
+class Input extends Component {
+    state = {
+        text: ""
+    }
+
+    onChange(e) {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        this.setState({
+            text: ""
+        })
+        this.props.onSendMessage(this.state.text)
+    }
+ 
+    render() {
+        return (
+            <div className="input">
+                <form onSubmit={ e => this.onSubmit(e)}>
+                <input 
+                    onChange={ e => this.onChange(e)}
+                    value={this.state.text}
+                    type="text"
+                    placeholder="Enter your message and press ENTER"
+                    autofocus="true"
+                />
+                <button>Send</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Input
