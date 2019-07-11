af7d75eed4e0e010d90f3501ea34d8132e957d71
Ashley <36495929+myashley@users.noreply.github.com>
2019-07-11 16:06:28 +1000

    Add Messages component to App.js

--- src/Messages.js
+++ src/Messages.js
@@ -1,20 +1,19 @@
-import React, {component} from "react"
-import { memberExpression } from "@babel/types";
+import React, {Component} from "react"
 
 class Messages extends Component {
 
     renderMessage(message) {
         const {member, text} = message
-        const {currrentMember} = this.props
-        const messageFromMe = member.id === currrentMember.id
+        const {currentMember} = this.props
+        const messageFromMe = member.id === currentMember.id
         const className = messageFromMe ? "Messages-message currentMember" : "Messages-message"
 
         return (
             <li className={className}>
-                <span className="avatar" style={{backgroundColor: member.clientData.color}} />
+                <span className="avatar" style={{backgroundColor: member.color}} />
                 <div className="Message-content">
                     <div className="username">
-                        {memberExpression.clientData.username}
+                        {member.username}
                     </div>
                     <div className="text">{text}</div>
                 </div>
